61178ffb925803589db58604b7aaee7822bbd2e5
Victor Castro-Cintas <vcastro-@student.42.fr>
2018-05-20 00:31:01 +0200

    improving 'Expertises' look

--- src/components/Home/Expertises.vue
+++ src/components/Home/Expertises.vue
@@ -1,63 +1,114 @@
 <template>
 	<div class="expertises" id="expertises">
 		<v-layout wrap>
-			<v-flex xs12>
+			<v-flex xs12 style="padding-left: 90px;">
 				<h2 class="home-title pb-5">EXPERTISES</h2>
 			</v-flex>
-			<v-flex xs12 md6 class="py-3">
-				<v-layout>
-					<v-flex shrink>
-						<img :src="require('../../assets/idea.png')"/>
+			<v-flex xs12>
+				<v-layout wrap>
+					<v-flex xs12 sm6 md4 class="py-5" style="background-color: #e6eaff;">
+						<v-layout>
+							<v-flex shrink class="px-4">
+								<img :src="idea"/>
+							</v-flex>
+							<v-flex class="expertise-text">
+								<div class="px-3">
+									<h4 class="primary--text">Mise en pages</h4>
+									<p>
+										Disposition graphique du contenu de la
+										maquette, hiérarchie des informations,
+										choix typographiques et création de
+										gabarit.
+									</p>
+								</div>
+							</v-flex>
+						</v-layout>
 					</v-flex>
-					<v-flex class="expertise-text">
-						<div class="px-5">
-							Réalisation sur-mesure de vos supports de
-							communication :<br/>
-							plaquette commerciale, catalogue, magazine, flyer,
-							brochure, affiche, publicité, dépliant, carte de visite,
-						</div>
+					<v-flex xs12 sm6 md4 class="py-5" style="background-color: #f2f4ff;">
+						<v-layout>
+							<v-flex shrink class="pl-4 pr-3">
+								<img :src="idea"/>
+							</v-flex>
+							<v-flex class="expertise-text">
+								<div class="px-3">
+									<h4 class="primary--text">Retouche d'images</h4>
+									<p>
+										Retouche et correction d’images,
+										chromie, ajustement des courbes et détour
+										age de haute qualité avec masque de fusion.
+									</p>
+								</div>
+							</v-flex>
+						</v-layout>
 					</v-flex>
-				</v-layout>
-			</v-flex>
-			<v-flex xs12 md6 class="py-3">
-				<v-layout>
-					<v-flex shrink>
-						<img :src="require('../../assets/idea.png')"/>
+					<v-flex xs12 sm6 md4 class="py-5" style="background-color: #e6eaff;">
+						<v-layout>
+							<v-flex shrink class="pl-4 pr-3">
+								<img :src="idea"/>
+							</v-flex>
+							<v-flex class="expertise-text">
+								<div class="px-3">
+									<h4 class="primary--text">Identité visuelle</h4>
+									<p>
+										Conception d’identité visuelle, charte
+										graphique, logo, en prenant en compte
+										le message que doit véhiculer la marque.
+									</p>
+								</div>
+							</v-flex>
+						</v-layout>
 					</v-flex>
-					<v-flex class="expertise-text">
-						<div class="px-5">
-							Création de votre logo, charte graphique, identité
-							visuelle ainsi que l’ensemble de leurs déclinaisons sur
-							les différents supports imprimés et en ligne.
-						</div>
+					<v-flex xs12 sm6 md4 class="py-5" style="background-color: #dae1ff;">
+						<v-layout>
+							<v-flex shrink class="pl-4 pr-3">
+								<img :src="idea"/>
+							</v-flex>
+							<v-flex class="expertise-text">
+								<div class="px-3">
+									<h4 class="primary--text">Supports de communication</h4>
+									<p>
+										Création de l’ensemble des supports
+										de communication (flyer, plaquette,
+										dépliant, brochure, carte de visite, affiche...)
+									</p>
+								</div>
+							</v-flex>
+						</v-layout>
 					</v-flex>
-				</v-layout>
-			</v-flex>
-			<v-flex xs12 md6 class="py-3">
-				<v-layout>
-					<v-flex shrink>
-						<img :src="require('../../assets/idea.png')"/>
+					<v-flex xs12 sm6 md4 class="py-5" style="background-color: #e6eaff;">
+						<v-layout>
+							<v-flex shrink class="pl-4 pr-3">
+								<img :src="idea"/>
+							</v-flex>
+							<v-flex class="expertise-text">
+								<div class="px-3">
+									<h4 class="primary--text">Design éditorial</h4>
+									<p>
+										Mise en place de charte graphique,
+										création de maquette pour tous types de
+										supports imprimés (livres, magazine,
+										catalogue...)
+									</p>
+								</div>
+							</v-flex>
+						</v-layout>
 					</v-flex>
-					<v-flex class="expertise-text">
-						<div class="px-5">
-							Réalisation de kakémono, d’habillage vitrine et de
-							visuel publicitaire qui s’adapte aux contraintes de
-							l’environnement.
-						</div>
-					</v-flex>
-				</v-layout>
-			</v-flex>
-			<v-flex xs12 md6 class="py-3">
-				<v-layout>
-					<v-flex shrink>
-						<img :src="require('../../assets/idea.png')"/>
-					</v-flex>
-					<v-flex class="expertise-text">
-						<div class="px-5">
-							Suivi de projet et préparation des fichiers pour
-							l’impression (résolution, taux d’encrage, traits de coupe
-							et fonds perdus).
-						</div>
+					<v-flex xs12 sm6 md4 class="py-5" style="background-color: #dae1ff;">
+						<v-layout>
+							<v-flex shrink class="pl-4 pr-3">
+								<img :src="idea"/>
+							</v-flex>
+							<v-flex class="expertise-text">
+								<div class="px-3">
+									<h4 class="primary--text">Reproduction de logo</h4>
+									<p>
+										Reproduction fidèle de logo existant,
+										pour qu’il puisse être exploiter pour tous
+										types de formats sans perte de qualité
+									</p>
+								</div>
+							</v-flex>
+						</v-layout>
 					</v-flex>
 				</v-layout>
 			</v-flex>
@@ -70,13 +121,13 @@ import { Vue, Component } from 'vue-property-decorator'
 
 @Component
 export default class Expertises extends Vue {
-
+	idea: any = require('../../assets/idea.png')
 }
 </script>
 
 <style scoped>
 	.expertises {
-		padding: 90px;
+		padding-top: 90px;
 	}
 
 	.expertise-text {
@@ -87,5 +138,25 @@ export default class Expertises extends Vue {
 		position: relative;
 		top: 50%;
 		transform: translateY(-50%);
+	}
+
+	img {
+		height: 100px;
+		position: relative;
+		top: 50%;
+		transform: translateY(-50%);
+	}
+
+	.expertise-text h4 {
+		text-transform: uppercase;
+	}
+
+	.expertise-text p::after {
+		content: '';
+		width: 30px;
+		height: 2px;
+		margin: 0 10px;
+		background-color: #0031FF;
+		display: inline-block;
 	}
 </style>
